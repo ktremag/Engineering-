aa1ee505a5062de71e7ea957f0076be28a93c9cc
ktremag <ktremaglio@gmail.com>
2019-12-09 09:58:10 -0500

    Added comments to increase clarity

--- Intermediate_Arduino/photoint/photo_int.ino
+++ Intermediate_Arduino/photoint/photo_int.ino
@@ -1,34 +1,29 @@
 #include "Arduino.h"
-int ledPin = 10;
+int ledPin = 10; // led pin
 int pintPin1 = 2;  //photo interrupter pin 1
 int pintPin2 = 3;
-int t =0;
-int state= 0;
+int t =0; // number of times passed
 
 void setup()
 {
 
-  	pinMode(ledPin, OUTPUT);
-    pinMode(pintPin1, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+    pinMode(pintPin1, INPUT_PULLUP); //setting up the photointerruptor
     pinMode(pintPin2, INPUT_PULLUP);
-  	Serial.begin(9600);
-  	attachInterrupt(digitalPinToInterrupt(pintPin2), ledOn, RISING);
-  	  	attachInterrupt(digitalPinToInterrupt(pintPin1), ledOff, FALLING);
+    Serial.begin(9600);
+    attachInterrupt(digitalPinToInterrupt(pintPin2), ledOn, RISING); // turining on LED when pass through
+    attachInterrupt(digitalPinToInterrupt(pintPin1), ledOff, FALLING); //turning off led when out 
 
   }
 
 void ledOn (){
-	digitalWrite(ledPin, HIGH);
-	t++;
+	digitalWrite(ledPin, HIGH); //led on 
+	t++; //adding to the count
 }
 void ledOff(){
-	digitalWrite(ledPin, LOW);
-
+	digitalWrite(ledPin, LOW); //led off
 }
-
 void loop (){
-	
-	Serial.println(t);
+	Serial.println(t); //printing count
 	delay(100);
-
 }
