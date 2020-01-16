cdcf535d3dc3cd0102451b64ca92f832cee9d85f
ktremag <ktremaglio@gmail.com>
2020-01-16 09:57:25 -0500

    Added comments

--- Intermediate_Arduino/motor_potent/motor_potent.ino
+++ Intermediate_Arduino/motor_potent/motor_potent.ino
@@ -1,20 +1,27 @@
-//#include "Arduino.h"
-
-int potPin = 3;
-int traPin = 9; 
+/* 
+Kit Tremaglio
+Motor Control 
+Intermediate Arduino
+Fall 2019
+*/
+int potPin = 3; //potentiometer pin
+int traPin = 9; //motor pin
 int t = 0;
     
 void setup()
 {
+	//set up input & output
   	pinMode(potPin, INPUT);
   	pinMode(traPin, OUTPUT);
   	Serial.begin(9600);} 
   
 void loop()
 {
+	//read amount of potentiometer and adjust to be able to be read by motor
 	t = (analogRead(potPin))/4;
+	//make the motor go at the rate set by the potentiometer
 	analogWrite(traPin, t);  
-	delay(200);
+	delay(200); //wait
 	Serial.println(t);
 } 
 
