0d8fc7a4535aac86c99095fb93035d8be1acff9b
ktremag <ktremaglio@gmail.com>
2020-01-16 09:51:45 -0500

    Added comments in void loop

--- Intermediate_Arduino/lcd_backpack/lcd_backpack.ino
+++ Intermediate_Arduino/lcd_backpack/lcd_backpack.ino
@@ -7,17 +7,20 @@ LiquidCrystal_I2C lcd (0x3F, 2, 1, 0, 4, 5, 6, 7);	//I2C address of the lcd back
 int t = 0;
 int buttonPin = 10;// what the button is plugged into
 int buttonState = 0;//if button is being pressed
-int switchPin = 11;
+int switchPin = 11; //pin of the switch
 int switchState = 0;
 int previous;
 
 void setup()
 {
+
+  //LCD set up and printing hello world	
   lcd.begin(16, 2); //16 * 2 LCD Module
   lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("hello world");
+  lcd.print("hello world"); 
+  //setup for inputs + pins	
   pinMode(buttonPin, INPUT);
   pinMode(switchPin, INPUT);
   Serial.begin(9600);
@@ -26,19 +29,23 @@ void setup()
 
 void loop() 
 {
+	//reading if switch is turned or button pressed
 	buttonState = digitalRead(buttonPin);
 	switchState = digitalRead(switchPin);
+	//serial monitor printing the switchstate
 	Serial.println(switchState);
+	// if the button is being pressed (but not continuous) and the switch is flipped add to the count displayed
 	if (buttonState == HIGH && buttonState != previous && switchState == HIGH) {// if the buttton is pressed, add one to t and print
 	t++;
 	}
+	//if not, subtract from the count
 	else if (buttonState == HIGH && buttonState != previous && switchState == LOW){
 	t--;
 	}
-	
-	delay(100);
+	delay(100); //wait
+	//print count
   	lcd.setCursor(0,1);
   	lcd.print(t);
+	//reset previous
   	previous = buttonState;
-  	
 }
